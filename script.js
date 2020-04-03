74e70f7e4306c163b88b4b7c2cb25e055b70233c
corinke <36419390+corinke@users.noreply.github.com>
2020-04-03 10:10:12 +0100

    added 2nd modal

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ window.onclick = function(e){
     modal.style.display = "none"
   }
 };
+
+let modalBtnA = document.getElementById("modal-btnA")
+let modalA = document.querySelector(".modalA")
+let closeBtnA = document.querySelector(".close-btnA")
+modalBtnA.onclick = function(){
+  modalA.style.display = "block"
+};
+closeBtnA.onclick = function(){
+  modalA.style.display = "none"
+};
+window.onclick = function(e){
+  if(e.target == modalA){
+    modalA.style.display = "none"
+  }
+};
